189fda050da322088f1272c9f25ba59043d7a7de
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-09-19 00:23:02 -0400

    Framework. Landing page ready for mock and change. ZURB Fundation and CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
-
+/*********************************** Variables *******************************************/
+var btnRandom = document.querySelector("#generateBtn");
+var btnCategory = document.querySelector("#SearchBtn");
 var divHeader = document.querySelector("#header");
 var doc = document.body;
 var i = 0;
 
+/*****************************************************************************************/
+
+/*********************************** Functions *******************************************/
+
 var changeBg = function() {
-  var color = ["#a5d2ff", "#aed6ff" , "#b7dbff", "#c0dfff", "#c9e4ff", "#d2e8ff", "#dbedff","#bedfff", "#d8ebff", "#f1f8ff"];  
+  var color = ["#a5d2ff", "#aed6ff" , "#b7dbff", "#c0dfff", "#c9e4ff", 
+                "#d2e8ff", "#dbedff","#bedfff", "#d8ebff", "#f1f8ff"];  
   divHeader.style.backgroundColor = color[i];
   doc.style.backgroundColor = color[i];
   i++;
@@ -14,4 +21,16 @@ var changeBg = function() {
   }
 };
 
+/*****************************************************************************************/
+
+/*********************************** Execution *******************************************/
+
 setInterval(changeBg, 1000);
+
+// adding listener for random restaurant
+
+
+// adding listener for category restaurant
+
+
+/*****************************************************************************************/
