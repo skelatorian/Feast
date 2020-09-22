713b9494b93b2cfc8b9f130446e9f6f5b3a4d23d
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-09-22 18:09:37 -0400

    Framework. Added the navBar. and other functionality for searching by city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 /*********************************** Variables *******************************************/
 var popup = new Foundation.Reveal($('#myModal'));
 
+
 /*****************************************************************************************/
 
 /*********************************** Functions *******************************************/
@@ -19,24 +20,42 @@ var displayModal = function(title, str){
 // listener for clicking on button Generate and search in jquery
 $(".btn").on("click", function() {
 
-    //getting the attribute of the button clicked
+    //getting the attribute of the button clicked whether it is random, searchin by city or category
     var search = $(this).attr("attr");
 
     //getting the value of the select tag
     var category = document.querySelector("#category").value || "";
+    var city = document.querySelector("#citySearch").value;
     
-    if(search === "random"){
+    if(search === "city"){
+      if (city) {
+        document.location.replace("./main.html?q=city");
+      }
+      else{
+        //Display modal for error
+      
+        displayModal("Search by City", "You must enter a City");
+      }
+
+    } else if(search === "random"){
       document.location.replace("./main.html?q=random");
     }
-    else if (search === "category" && category){
-      document.location.replace("./main.html?q="+category);
-    }
-    else{
+    else if (search === "category"){
+      if (category){
+        document.location.replace("./main.html?q="+category);
+      }
+      else{
       //Display modal for error
       
-      displayModal("Category", "You must select one Category")
+      displayModal("Search by Category", "You must select one Category");
       
+      }
     }
+    
+  });
+
+  $(document).ready(function() {
+    $(document).foundation();
   });
 
 
