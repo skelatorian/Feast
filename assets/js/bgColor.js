a77201f50d46b3e620f8b8f686a10db51803bb96
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-09-22 20:51:06 -0400

    Framework. Added main.html as a framework for the response

--- assets/js/bgColor.js
+++ assets/js/bgColor.js
@@ -1,6 +1,8 @@
 /*********************************** Variables *******************************************/
 
-var divHeader = document.querySelector("#header");
+var divHeader = document.querySelector(".header");
+var divLeft = document.querySelector(".top-bar-left");
+var divRight = document.querySelector(".top-bar-right");
 var doc = document.body;
 var i = 0;
 
@@ -12,6 +14,8 @@ var changeBg = function() {
   var color = ["#a5d2ff", "#aed6ff" , "#b7dbff", "#c0dfff", "#c9e4ff", 
                 "#d2e8ff", "#dbedff","#bedfff", "#d8ebff", "#f1f8ff"];  
   divHeader.style.backgroundColor = color[i];
+  divRight.style.backgroundColor = color[i];
+  divLeft.style.backgroundColor = color[i];
   doc.style.backgroundColor = color[i];
   i++;
   
